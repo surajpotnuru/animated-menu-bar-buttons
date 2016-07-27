619975da99ac2f8a958a8bcbc2bc3989aeb412fa
suraj <suraj.potnuru@stichio.in>
2016-07-27 14:10:44 +0530

    2 Buttons

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 /**
  * Created by suraj on 27/7/16.
  */
-$(document).ready(function(){
-   $(".menu-button-1").click(function(e){
-       if($(".menu-button-1 .bar").hasClass("open")){
-           $(".menu-button-1 .bar").removeClass("open");
-       }else{
-           $(".menu-button-1 .bar").addClass("open");
-       }
-   });
-    $(".menu-button-2").click(function(e){
-       if($(".menu-button-2 .bar").hasClass("open")){
-           $(".menu-button-2 .bar").removeClass("open");
-       }else{
-           $(".menu-button-2 .bar").addClass("open");
-       }
-   });
+$(document).ready(function () {
+    $(".menu-button-1").click(function (e) {
+        if ($(".menu-button-1 .bar").hasClass("open")) {
+            $(".menu-button-1 .bar").removeClass("open");
+        } else {
+            $(".menu-button-1 .bar").addClass("open");
+        }
+    });
+    $(".menu-button-2").click(function (e) {
+        if ($(".menu-button-2 .bar").hasClass("open")) {
+            $(".menu-button-2 .bar").removeClass("open");
+        } else {
+            $(".menu-button-2 .bar").addClass("open");
+        }
+    });
 
-    setInterval(function(){
-        $(".menu-button-1").trigger("click");
-        $(".menu-button-2").trigger("click");
-    },1000);
+
+    var animating = false;
+    var animation;
+
+    $('.start-animating-all').click(function(){
+        if (animating) {
+            clearInterval(animation);
+            animating = false;
+            $('.start-animating-all').html("Start Animating All Buttons");
+        } else {
+            animation = setInterval(function () {
+                $(".menu-button-1").trigger("click");
+                $(".menu-button-2").trigger("click");
+            }, 1000);
+            animating = true;
+            $('.start-animating-all').html("Stop Animating");
+        }
+    });
 
 });
